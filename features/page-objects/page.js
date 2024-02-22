bbf7ea82e84efd195aba3249f1bd34b266f7a36f
Irina <irinafedjko@gmail.com>
2024-02-22 16:49:15 +0200

    new scenarious

--- features/page-objects/page.js
+++ features/page-objects/page.js
@@ -1,6 +1,6 @@
-
-
-export class Page{
+export class Page {
+    get cartButton() { return $('.showcart'); }
+    get proceedToCheckoutButton() { return $('#top-cart-btn-checkout'); }
 }
 
 export default new Page();
---
+++ features/test-data/users.js
@@ -0,0 +1,8 @@
+export const users = {
+    Lana: {
+        email: "aafedko@gmail.com",
+        password: "aafedko123@",
+        firstname: "Lana",
+        lastname: "Lana"
+    }
+};
